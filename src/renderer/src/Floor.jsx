cb23deebb3265b1f9c1fe55f8b24dedbf5579598
anuvette <thisisanuxoxo@gmail.com>
2024-02-08 22:32:12 +0545

    minor quality of life update; keyboard accessibility

--- src/renderer/src/Floor.jsx
+++ src/renderer/src/Floor.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import './assets/Floor.css'
 import AddNewFloorButton from './AddNewFloorButton'
@@ -17,9 +17,21 @@ const Floor = () => {
   })
 
   const handleFloorChange = (event) => {
-    const floorNumber = event.target.id.replace('Floor', '')
-    setActiveFloor(floorNumber)
+    if (event.type === 'change') {
+      const floorNumber = event.target.id.replace('Floor', '')
+      setActiveFloor(floorNumber)
+    } else if (event.type === 'keydown' && event.key >= '1' && event.key <= '4') {
+      setActiveFloor(event.key)
+    }
   }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleFloorChange)
+
+    return () => {
+      window.removeEventListener('keydown', handleFloorChange)
+    }
+  }, [])
 
   if (floorQuery.isLoading) {
     return (
@@ -59,7 +71,8 @@ const Floor = () => {
               type="radio"
               name="Floor"
               onClick={handleFloorChange}
-              defaultChecked
+              onChange={handleFloorChange}
+              checked={activeFloor === '1'}
             />
             <label htmlFor="Floor1">Floor 1</label>
           </div>
@@ -67,21 +80,39 @@ const Floor = () => {
 
         <div className="mother-container">
           <div className="input-container">
-            <input id="Floor2" type="radio" name="Floor" onClick={handleFloorChange} />
+            <input
+              id="Floor2"
+              type="radio"
+              name="Floor"
+              onChange={handleFloorChange}
+              checked={activeFloor === '2'}
+            />
             <label htmlFor="Floor2">Floor 2</label>
           </div>
         </div>
 
         <div className="mother-container">
           <div className="input-container">
-            <input id="Floor3" type="radio" name="Floor" onClick={handleFloorChange} />
+            <input
+              id="Floor3"
+              type="radio"
+              name="Floor"
+              onChange={handleFloorChange}
+              checked={activeFloor === '3'}
+            />
             <label htmlFor="Floor3">Floor 3</label>
           </div>
         </div>
 
         <div className="mother-container">
           <div className="input-container">
-            <input id="Floor4" type="radio" name="Floor" onClick={handleFloorChange} />
+            <input
+              id="Floor4"
+              type="radio"
+              name="Floor"
+              onChange={handleFloorChange}
+              checked={activeFloor === '4'}
+            />
             <label htmlFor="Floor4">Floor 4</label>
           </div>
         </div>
